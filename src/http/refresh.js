5bf25259943a8d53182971eb930e6ee0d18c189d
xiaoxixideyu <yu153957@protonmail.com>
2024-01-10 01:07:22 +0800

    fix: refresh token add a threshold

--- src/http/refresh.js
+++ src/http/refresh.js
@@ -1,8 +1,10 @@
 export { AUTH } from "@/common/constants.js"
 import { useUserStore } from "@/store/user"
 
-let subscribes=[]
-let flag=false // 设置开关，保证 refresh token 响应前不会被重复执行
+let subscribes =[ ]
+let refreshing = false // 设置开关，保证 refresh token 响应前不会被重复执行
+let refreshingCnt = 0
+const refreshingThreshold = 10
 
 export const addRequest = (request) => {
     subscribes.push(request)
@@ -14,15 +16,19 @@ export const retryRequest = () => {
 }
 
 export const refreshToken = () => {
-    if (!flag) {
-        flag = true
+    if (!refreshing || refreshingCnt >= refreshingThreshold) {
+        refreshing = true
         const userStore = useUserStore()
         userStore.tryRefreshToken().then(() => {
-            flag = false
+            refreshing = false
+            refreshingCnt = 0
             // 重新请求数据
             retryRequest()
         }).catch(() => {
-            flag = false
+            refreshing = false
+            refreshingCnt = 0
         })
+    } else {
+        refreshingCnt++
     }
 }
